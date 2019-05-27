8ba56c1fe90e2e09e4f09d510f040c4155ee38a2
tiago maffei <tiagomaffei@tiagos-MacBook-Pro.local>
2019-05-27 12:08:20 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import CurrencyConverter from './Components/CurrencyConverter'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-
-      </div>
-    );
-  }
+	render() {
+		return (
+			<>
+				<h2>Render Props</h2>
+				<CurrencyConverter
+					render={(currencyData, amount) => (
+						<p>
+							US Dollar ${amount.toFixed(2)} - {currencyData.name}{' '}
+							{currencyData.symbol}
+							{(amount * currencyData.rate).toFixed(2)}
+						</p>
+					)}
+				/>
+			</>
+		)
+	}
 }
 
-export default App;
+export default App
